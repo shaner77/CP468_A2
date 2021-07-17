bf21290157b3c5fa3aa231ae81fbf9c3d02c04ef
shaner77 <shane-7@live.ca>
2021-07-17 15:54:00 -0400

    Add files via upload

---
+++ Question2.py
@@ -0,0 +1,21 @@
+import csv 
+import matplotlib.pyplot as plt
+
+try:
+    with open('datasetQ2.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            line = ','.join(row)
+            if line_count == 0:
+                print(f'Features:\n {"  ,  ".join(row)}')
+                line_count += 1
+            else:
+                a = line.split(',', 1)
+                plt.scatter(float(a[0]), float(a[1]))
+                print(a)
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+    plt.show()
+except:
+    print("something broke")
