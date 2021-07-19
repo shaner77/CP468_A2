c72aa1bd30345e0d42fa1ba5f0616a6a06e48890
shaner77 <shane-7@live.ca>
2021-07-19 01:15:02 -0400

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import math
 from time import sleep
 
+
+
 def main():
 
     centroid1 = [20.0, 40.0]
@@ -24,33 +26,34 @@ def main():
     
     different = True
     b = [0,0]
-
+    iteration = 0
+    
     try:
         plt.ion()
         while different == True:
-            '''start loop above'''
             with open('datasetQ2.csv', mode='r') as csv_file:
+                
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 line_count = 0
              
                 for row in csv_reader:
-
                     if line_count == 0:
-                        print(f'Features: {"  ,  ".join(row)}')
+                        #print(f'Features: {"  ,  ".join(row)}')
                         line_count += 1            
                     else:                   
-                        line = ','.join(row)
                         
                         plt.scatter(float(centroid1[0]), float(centroid1[1]), color="blue")
                         plt.scatter(float(centroid2[0]), float(centroid2[1]), color="blue")
+                        plt.annotate('Centroid1', (float(centroid1[0]), float(centroid1[1])), color="blue")
+                        plt.annotate('Centroid2', (float(centroid2[0]), float(centroid2[1])), color="blue")
+                        
+                        line = ','.join(row)
                         a = line.split(',', 1)
                         b = [float(a[0]), float(a[1])]
                         e1 = euclid(centroid1, b)
                         e2 = euclid(centroid2, b)
-                       
-                        print(f'line({line_count}) {b[0]} | {b[1]}')
-                        if(e1 <= e2):
 
+                        if(e1 <= e2):
                             plt.scatter(b[0], b[1], color="red")
                             x_values = [b[0], centroid1[0]]
                             y_values = [b[1], centroid1[1]]
@@ -92,9 +95,13 @@ def main():
                                 elif b[0] > max2x:
                                     max2x = b[0]
                                 elif b[1] > max2y:
-                                    max2y = b[1]           
+                                    max2y = b[1]
+                                    
                         line_count += 1
 
+                print("========================================================") 
+                print(f'[{iteration}]  centroid1 [ {centroid1[0]} , {centroid1[1]} ] | centroid2 [ {centroid2[0]} , {centroid2[1]} ]')
+                #plt.autoscale(True, 'both')     #this makes the graph view scaled to the points
                 plt.xlim(0, 80)
                 plt.ylim(0, 80)
                 if ((oldcent1[0] == centroid1[0]) and (oldcent1[1] == centroid1[1]) and (oldcent2[0] == centroid2[0]) and (oldcent2[1] == centroid2[1])):
@@ -104,17 +111,28 @@ def main():
                     oldcent2 = centroid2
                     centroid1 = [(max1x + min1x)/2, (max1y + min1y)/2]
                     centroid2 = [(max2x + min2x)/2, (max2y + min2y)/2]
-                plt.pause(0.25)
+                
                 if different != False:
+                    plt.pause(0.5)
                     plt.cla()
+                else:
+                    plt.pause(2)
+                    plt.close('all')
+                    print("========================================================") 
+                    print("[Final]")
+                    print(f'(RED) Centroid1 : {centroid1}  |  Cluster Size: {redcount} \n(Blk) Centroid2 : {centroid2}  |  Cluster Size: {blkcount}')
+
             redcount = 0
             blkcount = 0
+            iteration += 1
+            
         plt.ioff()
         plt.show()
         
+                
     except:
-        print("something broke")
-
+        print("Something isn't working")
+        
 def euclid(cent, A):
     
     x = ((A[0] - cent[0])**2)
